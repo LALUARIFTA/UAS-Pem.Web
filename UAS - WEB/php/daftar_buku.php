64e04da6dc7662abf18e223760fc88184bfbbce3
lalu arif trasna ashari <93263857+LALUARIFTA@users.noreply.github.com>
2023-07-03 21:24:40 +0800

    Add files via upload

---
+++ UAS - WEB/php/daftar_buku.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="Style.css">
+<title>Perpustakaan Online</title>
+</head>
+<body>
+    
+<div id="container">
+<div class="header"><img src="images/perp.jpg" width="1000" height="300"><h1>Perpustakaan Universitas BSI</h1></div>
+<h4>Selamat Datang, Tiyo</h4>
+
+<div class="main">
+<div class="left">
+<h3 align="center">MENU</h3>
+<ul>
+<li><a href="index.php">Logout</a></li>
+<li><a href="home.php">Home</a></li>
+<li><a href="#">Daftar Buku</a></li>
+</ul>
+
+<br>
+
+<h3 align="center">BUKU TERPOPULER</h3>
+<ul>
+<li><a href="#">Web Design</a></li>
+<li><a href="#">Pemrograman</a></li>
+<li><a href="#">Database</a></li>
+</ul>
+</div>
+</div>
+
+<div class="middle2">
+<h2 align="center">DAFTAR BUKU</h2><br>
+<div align="center">
+</div>
+
+<div class="buku">
+<div class="foto">
+<img src="images/rahvayana.jpeg">
+<div class="judul">Rahvayana - Aku Lala Padamu</div>
+<div class="penulis">Penulis: Sujiwo Tejo</div>
+</div>
+</div>
+
+<div class="buku">
+<div class="foto">
+<img src="images/Dermaga.jpeg">
+<div class="judul">Dermaga Sastra Indonesia</div>
+<div class="penulis">Penulis: Jamal D. Rahman</div>
+</div>
+</div>
+
+<div class="buku">
+<div class="foto">
+<img src="images/Pemrograman.jpeg">
+<div class="judul">Pemrograman Android & Database</div>
+<div class="penulis">Penulis: Abdul Kadir</div>
+</div>
+</div>
+
+<div class="buku">
+<div class="foto">
+<img src="images/Seni.jpeg">
+<div class="judul">Sebuah Seni Untuk Bersikap Bodo Amat</div>
+<div class="penulis">Penulis: Mark Manson</div>
+</div>
+</div>
+</div>
+
+
+<div class="middle2">
+<div class="buku">
+<div class="foto">
+<img src="images/Menjadi.jpeg">
+<div class="judul">Menjadi Web Entrepreneur, Siapa takut?</div>
+<div class="penulis">Penulis: Su Rahman</div>
+</div>
+</div>
+
+<div class="buku">
+<div class="foto">
+<img src="images/Jaringan.jpeg">
+<div class="judul">Jaringan Komputer Berbasis Mikrotik +Cd</div>
+<div class="penulis">Penulis: Iwan Sofana</div>
+</div>
+</div>
+
+<div class="buku">
+<div class="foto">
+<img src="images/BukuSakti.jpeg">
+<div class="judul">Buku Sakti Pemrogaman Web</div>
+<div class="penulis">Penulis: Didik Setiawan</div>
+</div>
+</div>
+
+<div class="buku">
+<div class="foto">
+<img src="images/MahirDesain.jpeg">
+<div class="judul">Mahir Desain Grafis dengan CorelDraw</div>
+<div class="penulis">Penulis: Jubilee Enterprise</div>
+</div>
+</div>
+
+</div>
+</div>
+<div class="footer"><p align="center">Copyright © 2023 - Kelompok Prikitiew</a></p>
+</div>
+</body>
+</html>
